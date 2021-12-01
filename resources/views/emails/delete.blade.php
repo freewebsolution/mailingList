8dc4a0aa7874f9814a51230f6ad3224630dfb7f1
Lucio <info@freewebsolution.it>
2021-12-01 13:18:49 +0100

    Modificato layout delete --Lucio

--- resources/views/emails/delete.blade.php
+++ resources/views/emails/delete.blade.php
@@ -1,15 +1,16 @@
 @extends('master')
 @section('title', 'Delete email')
 @section('content')
-<div>
-    <p class="text-muted">Oh no! {{$email->email}} ci dispiace che ci lasci!!</p>
-</div>
-<form method="post" action="{{action([\App\Http\Controllers\MailingController::class,'destroy' ],$email->id)}}" class="float-left mr-2">
-    @csrf
-    <div>
-        <button class="btn btn-warning">
-            Unsubscribe
-        </button>
+    <div class="conteiner mx-auto">
+        <p class="text-muted">Oh no! {{$email->email}} ci dispiace che ci lasci!!</p>
+
+        <form method="post" action="{{action([\App\Http\Controllers\MailingController::class,'destroy' ],$email->id)}}" class="float-left mr-2">
+            @csrf
+
+            <button class="btn btn-warning">
+                Unsubscribe
+            </button>
+        </form>
     </div>
-</form>
+
 @endsection
