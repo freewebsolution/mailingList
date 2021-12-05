491e190fdfd944dd2b510a26888ba872b67be10a
Lorenzo <lopadova@users.noreply.github.com>
2021-12-05 18:41:21 +0100

    Update MailSendService.php

--- app/service/MailSendService.php
+++ app/service/MailSendService.php
@@ -3,15 +3,22 @@
 namespace App\service;
 
 use Illuminate\Support\Facades\Mail;
-
+use App\Models\Mailing;
 
 class MailSendService
 {
-    public function send(array $data, string $email):void
+    public function send(Mailing $mail):void
     {
-        Mail::send('emails.mailing', $data, function ($msg) use ($data, $email) {
-            $msg->from('noreply@email.dev', 'Lucio Ticali');
-            $msg->to($email)->subject('Mailing list');
+        $data = [
+            'email' => $mail->email,
+            'id' => $mail->id
+        ];
+
+        Mail::send('emails.mailing', $data, function ($msg) use ($data, $mail) {
+            $msg->from('noreply@email.dev', 'Lucio Ticali')
+                ->to($mail->email)
+                ->subject('Mailing list')
+            ;
         });
     }
 
