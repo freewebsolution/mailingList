7bfea59995cef0452a3cf2d8a67e35cc0e6f02fe
Your Name <you@example.com>
2021-12-06 17:58:25 +0100

    Rivisto il from di Mail::send --Lucio

--- config/mailing.php
+++ config/mailing.php
@@ -1,7 +1,9 @@
 <?php
 
 return [
-    'emailFrom'=> env('EMAIL_FROM','noreply@gmail.com'),
-    'aliasFrom'=>env('ALIAS_FROM','Lucio Ticali'),
+    'from'=>[
+        'email'=>env('EMAIL_FROM','noreply@gmail.com'),
+        'name'=>env('ALIAS_FROM','Lucio Ticali'),
+    ],
     'subject'=>env('SUBJECT','Add our mailing list')
 ];
